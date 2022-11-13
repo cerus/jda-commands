c9e47caa29c98559bb4d90b854f5159439460a38
Cerus <github.social@cerus.dev>
2022-11-13 22:59:11 +0100

    Use method return type instead of instance type for dependency injection

--- src/main/java/com/github/kaktushose/jda/commands/dependency/DependencyInjector.java
+++ src/main/java/com/github/kaktushose/jda/commands/dependency/DependencyInjector.java
@@ -1,6 +1,12 @@
 package com.github.kaktushose.jda.commands.dependency;
 
 import com.github.kaktushose.jda.commands.annotations.Produces;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.reflections.Reflections;
@@ -10,13 +16,6 @@ import org.reflections.util.ConfigurationBuilder;
 import org.reflections.util.FilterBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Central registry for dependencies and producing methods. Registered dependencies will be injected with the
@@ -38,8 +37,8 @@ public class DependencyInjector {
      * Create a new DependencyInjector.
      */
     public DependencyInjector() {
-        providedObjects = new HashMap<>();
-        dependencies = new HashMap<>();
+        this.providedObjects = new HashMap<>();
+        this.dependencies = new HashMap<>();
     }
 
     /**
@@ -49,40 +48,40 @@ public class DependencyInjector {
      * @param packages package(s) to exclusively scan
      * @param clazz    a class of the classpath to scan
      */
-    public void index(@NotNull Class<?> clazz, @NotNull String... packages) {
-        log.debug("Indexing dependency providers...");
+    public void index(@NotNull final Class<?> clazz, @NotNull final String... packages) {
+        this.log.debug("Indexing dependency providers...");
 
-        FilterBuilder filter = new FilterBuilder();
-        for (String pkg : packages) {
+        final FilterBuilder filter = new FilterBuilder();
+        for (final String pkg : packages) {
             filter.includePackage(pkg);
         }
 
-        ConfigurationBuilder config = new ConfigurationBuilder()
+        final ConfigurationBuilder config = new ConfigurationBuilder()
                 .setScanners(Scanners.SubTypes, Scanners.MethodsAnnotated)
                 .setUrls(ClasspathHelper.forClass(clazz))
                 .filterInputsBy(filter);
-        Reflections reflections = new Reflections(config);
+        final Reflections reflections = new Reflections(config);
 
-        Set<Method> methods = reflections.getMethodsAnnotatedWith(Produces.class);
-        for (Method method : methods) {
+        final Set<Method> methods = reflections.getMethodsAnnotatedWith(Produces.class);
+        for (final Method method : methods) {
             if (method.getParameterTypes().length != 0) {
-                log.error("An error has occurred! Skipping Producer {}", method,
+                this.log.error("An error has occurred! Skipping Producer {}", method,
                         new IllegalArgumentException("Producer method must not have parameters!"));
                 continue;
             }
-            Object instance;
+            final Object instance;
             try {
                 instance = method.getDeclaringClass().getConstructors()[0].newInstance();
-            } catch (Exception e) {
-                log.error("Unable to create provider instance!", e);
+            } catch (final Exception e) {
+                this.log.error("Unable to create provider instance!", e);
                 continue;
             }
             try {
                 method.setAccessible(true);
-                Object object = method.invoke(instance);
-                providedObjects.put(object.getClass(), object);
-            } catch (Exception e) {
-                log.error("Unable to access method {}", method, e);
+                final Object object = method.invoke(instance);
+                this.providedObjects.put(method.getReturnType(), object);
+            } catch (final Exception e) {
+                this.log.error("Unable to access method {}", method, e);
             }
         }
     }
@@ -93,22 +92,22 @@ public class DependencyInjector {
      *
      * @param provider instance of the class to scan
      */
-    public void registerProvider(@NotNull Object provider) {
-        for (Method method : provider.getClass().getDeclaredMethods()) {
+    public void registerProvider(@NotNull final Object provider) {
+        for (final Method method : provider.getClass().getDeclaredMethods()) {
             if (!method.isAnnotationPresent(Produces.class)) {
                 continue;
             }
             if (method.getParameterTypes().length != 0) {
-                log.error("An error has occurred! Skipping Producer {}", method,
+                this.log.error("An error has occurred! Skipping Producer {}", method,
                         new IllegalArgumentException("Producer method must not have parameters!"));
                 continue;
             }
             try {
                 method.setAccessible(true);
-                Object object = method.invoke(provider);
-                providedObjects.put(object.getClass(), object);
-            } catch (Exception e) {
-                log.error("Unable to access method {}", method, e);
+                final Object object = method.invoke(provider);
+                this.providedObjects.put(object.getClass(), object);
+            } catch (final Exception e) {
+                this.log.error("Unable to access method {}", method, e);
             }
         }
     }
@@ -119,28 +118,28 @@ public class DependencyInjector {
      * @param instance instance of the declaring class
      * @param fields   the dependencies to register
      */
-    public void registerDependencies(@NotNull Object instance, @NotNull List<Field> fields) {
-        dependencies.put(instance, fields);
+    public void registerDependencies(@NotNull final Object instance, @NotNull final List<Field> fields) {
+        this.dependencies.put(instance, fields);
     }
 
     /**
      * Injects all registered dependencies with the corresponding value. If no value is present {@code null} gets injected.
      */
     public void inject() {
-        dependencies.forEach((instance, fields) -> {
-            for (Field field : fields) {
+        this.dependencies.forEach((instance, fields) -> {
+            for (final Field field : fields) {
                 try {
                     field.setAccessible(true);
-                    field.set(instance, getDependency(field.getType()));
-                } catch (IllegalAccessException e) {
-                    log.error("Unable to inject field {}", field, e);
+                    field.set(instance, this.getDependency(field.getType()));
+                } catch (final IllegalAccessException e) {
+                    this.log.error("Unable to inject field {}", field, e);
 
                 }
             }
         });
     }
 
-    private Object getDependency(@Nullable Class<?> clazz) {
-        return providedObjects.get(clazz);
+    private Object getDependency(@Nullable final Class<?> clazz) {
+        return this.providedObjects.get(clazz);
     }
 }
